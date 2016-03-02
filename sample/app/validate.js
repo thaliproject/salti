12c60c7b49582f2181ed70c85a841368bc56d634
cicorias <github@cicoria.com>
2016-03-02 16:01:46 -0500

    added working sample

---
+++ sample/app/validate.js
@@ -0,0 +1,44 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('ValidateController', ['$scope', '$http', function ($scope, $http) {
+  $scope.greeting = 'Welcome to SALTI!';
+  $scope.errors = 'no errors';
+  $scope.messages = 'no messages';
+  $scope.secret = 'foobar';
+  $scope.newdoc = genDoc();
+
+  $scope.createDoc = function () {
+    
+    var body = { 'secret': 'CLEAR ' + $scope.secret, 'doc' : $scope.newdoc};
+    
+    $http({
+      method: 'POST',
+      url: '/validate',
+      data: body
+    }).then(function successCallback(response) {
+      $scope.newdoc = genDoc();
+      if (response.data.secret)
+        $scope.secret = response.data.secret;
+      else
+        $scope.messages = JSON.stringify(response);
+    }, function errorCallback(response) {
+      $scope.errors = JSON.stringify(response);
+    });
+  }
+
+}]);
+
+  
+function genDoc(){
+  return JSON.stringify({ _id: guid(), "type": "foobar" })
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
--- sample/server.js
+++ sample/server.js
@@ -2,11 +2,29 @@
 'use strict';
 
 var express = require('express'),
+  http = require('http'),
   app = express(),
   PouchDB = require('pouchdb'),
-  router = express.Router();
+  router = express.Router(),
+  debug = require('debug')('thalisalti:express');
+
+
+//this is our sample UI site on port http://localhost:3000
+// the 2 express apps:
+var webApp = require('./app');
+var webAppPort = normalizePort(process.env.PORT || '3000');
+
+webApp.set('port', webAppPort);
+var webServer = http.createServer(webApp);
+webServer.listen(webAppPort, function () {
+  debug('webServer is listening on port %s', webAppPort);
+});
+webServer.on('error', onError);
+
+
 
 var pbsetup = PouchDB.defaults({ prefix: './db/' });
+var pouchPort = normalizePort(process.env.PORT2 || '3001');
 
 var opts = {
   mode: 'minimumForPouchDB',
@@ -25,5 +43,45 @@ var pouchApp = require('express-pouchdb')(pbsetup, opts);
 router.use('/', pouchApp);
 app.use('/', router);
 
-app.listen(process.env.PORT || 3000);
+app.listen(pouchPort);
 
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error, parent) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // var bind = typeof port === 'string'
+  //   ? 'Pipe ' + port
+  //   : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error('port requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error('port is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
